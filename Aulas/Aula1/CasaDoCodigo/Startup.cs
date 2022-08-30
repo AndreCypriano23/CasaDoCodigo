e8e4d3ada596d902f33afba3b5558a13d776bee7
AndreCypriano23 <andrecypriano23@outlook.com>
2022-08-30 09:38:25 -0300

    Primeiro Commit

---
+++ Aulas/Aula1/CasaDoCodigo/Startup.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CasaDoCodigo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            string connectionString = Configuration.GetConnectionString("Default");
+
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(connectionString)
+            );
+
+            //Instancia que vai existir somente quando os objetos que usam essa instância estará ativo
+            services.AddTransient<DataService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+            IServiceProvider serviceProvider)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Pedido}/{action=Carrossel}/{id?}");
+            });
+
+            serviceProvider.GetService<DataService>().InicializaDB();
+                   
+        }
+    }
+
+    class DataService
+    {
+        private readonly ApplicationContext contexto;
+
+        public DataService(ApplicationContext contexto)
+        {
+            this.contexto = contexto;
+        }
+
+        public void InicializaDB()
+        {
+            //Ele vai criar o BD se necessário
+            contexto.Database.EnsureCreated();
+        }
+    }
+}
