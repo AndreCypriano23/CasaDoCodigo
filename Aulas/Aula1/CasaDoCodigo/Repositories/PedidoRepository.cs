2843135d3a021f1eb0cff60cd3db5fb6d4e72ed2
AndreCypriano23 <andrecypriano23@outlook.com>
2022-11-02 09:14:50 -0300

    Session Implementada

--- Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
+++ Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -1,4 +1,5 @@
 ﻿using CasaDoCodigo.Models;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +14,23 @@ namespace CasaDoCodigo.Repositories
 
     public class PedidoRepository : BaseRepository<Pedido>, IPedidoRepository
     {
-      
-        public PedidoRepository(ApplicationContext contexto) : base(contexto)
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public PedidoRepository(ApplicationContext contexto, IHttpContextAccessor contextAccessor)
+            : base(contexto)
         {
+            this._contextAccessor = contextAccessor;
         }
+
+        private int? GetPedidoId()
+        {
+            return _contextAccessor.HttpContext.Session.GetInt32("pedidoId");//lendo => mesmo nome que coloquei no set 'pedidoId'
+        }
+
+        private void SetPedidoId(int pedidoId)
+        {
+            _contextAccessor.HttpContext.Session.SetInt32("pedidoId", pedidoId);//Gravando 
+        }
+
     }
 }
