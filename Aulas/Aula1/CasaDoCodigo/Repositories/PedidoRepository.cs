7047b6723fa25c58167193a99d6e2b270867461f
AndreCypriano23 <andrecypriano23@outlook.com>
2022-11-02 15:09:48 -0300

    Salvando pedido no carrinho

--- Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
+++ Aulas/Aula1/CasaDoCodigo/Repositories/PedidoRepository.cs
@@ -9,7 +9,8 @@ namespace CasaDoCodigo.Repositories
 {
     public interface IPedidoRepository
     {
-
+        Pedido GetPedido();
+        void AddItem(string codigo);
     }
 
     public class PedidoRepository : BaseRepository<Pedido>, IPedidoRepository
@@ -20,6 +21,52 @@ namespace CasaDoCodigo.Repositories
             : base(contexto)
         {
             this._contextAccessor = contextAccessor;
+        }
+
+        public void AddItem(string codigo)
+        {
+            var produto = contexto.Set<Produto>().Where(p => p.Codigo == codigo)
+                .SingleOrDefault();
+
+            if(produto == null)
+            {
+                throw new ArgumentException("Produto não encontrado");
+            }
+
+            var pedido = GetPedido();
+            var itemPedido = contexto.Set<ItemPedido>()
+              .Where(i => i.Produto.Codigo == codigo && i.Pedido.Id == pedido.Id)
+              .SingleOrDefault();
+
+            if(itemPedido == null)
+            {
+                itemPedido = new ItemPedido(pedido, produto, 1, produto.Preco);
+                contexto.Set<ItemPedido>()
+                    .Add(itemPedido);
+
+                contexto.SaveChanges();
+            }
+
+        }
+
+        public Pedido GetPedido()
+        {
+            //Obter o Pedido Atual. Primeiro passo, saber qual é o Id do pedido que está gravado na sessao
+            var pedidoId = GetPedidoId();
+            var pedido = dbSet.Where(p => p.Id == pedidoId).SingleOrDefault();
+            //Igual ao Id da sessao. O SingleOrDefault retorna o valor ou um nulo se nao tiver, sem dar um erro
+                
+            if(pedido == null)
+            {
+                pedido = new Pedido();
+                //Add essa nova instância ao DbSet de produto
+                dbSet.Add(pedido);
+                contexto.SaveChanges();//grava no banco e obtém o novo Id p/ o pedido
+                //Gravando na sessao 
+                SetPedidoId(pedido.Id);//Agora vou aproveitar sempre o mesmo Id a cada navegação
+            }
+
+            return pedido; 
         }
 
         private int? GetPedidoId()
