d1b4dab24fe0c07d24265a44e84847b867698765
AndreCypriano23 <andrecypriano23@outlook.com>
2022-11-15 13:30:32 -0300

    Arquivos de Javascript organizados e funções de quantidade alteradas

--- Aulas/Aula1/CasaDoCodigo/Repositories/ItemPedidoRepository.cs
+++ Aulas/Aula1/CasaDoCodigo/Repositories/ItemPedidoRepository.cs
@@ -8,12 +8,30 @@ namespace CasaDoCodigo.Repositories
 {
     public interface IItemPedidoRepository
     {
-
+        void UpdateQuantidade(ItemPedido itemPedido);
+       
     }
+
     public class ItemPedidoRepository : BaseRepository<ItemPedido>, IItemPedidoRepository
     {
         public ItemPedidoRepository(ApplicationContext contexto) : base(contexto)
         {
+
+        }
+
+        public void UpdateQuantidade(ItemPedido itemPedido)
+        {
+            var itemPedidoDB = dbSet.
+                Where(ip => ip.Id == itemPedido.Id).FirstOrDefault();
+        
+            if(itemPedidoDB != null)
+            {
+                /* Aqui estava dando erro de não poder modificar a quantidade pq o set dele é privado, então fiz um método para poder alterar isso 
+                itemPedidoDB.AtualizaQuantidade(itemPedido.Quantidade) = itemPedido.Quantidade;*/
+                itemPedidoDB.AtualizaQuantidade(itemPedido.Quantidade);
+
+                contexto.SaveChanges();
+            }
         }
     }
 }
---
+++ Aulas/Aula1/CasaDoCodigo/wwwroot/js/carrinho.js
@@ -0,0 +1,65 @@
+﻿//Vou encapsular a classe dentro de um escopo
+
+class Carrinho {
+
+    clickIncremento(btn) {
+
+        let data = this.getData(btn);
+        data.Quantidade++;
+        this.postQuantidade(data);
+
+        //é um breakPoint debugger;
+        //debugger
+    }
+
+
+    clickDecremento(btn) { 
+
+        let data = this.getData(btn);
+        data.Quantidade--;
+        this.postQuantidade(data);
+
+    }
+
+    updateQuantidade(input) {
+
+        let data = this.getData(input);
+        this.postQuantidade(data);
+
+    }
+
+
+    getData(elemento) {
+        //vou usar o Jquery, que já vem instalado
+        var linhaDoItem = $(elemento).parents('[item-id]');//para pegar a div pai que fica logo depois do foreach
+        var itemId = $(linhaDoItem).attr('item-id');
+        var novaQtde = $(linhaDoItem).find('input').val();//localizar um elemento abaixo, um input
+
+         return {
+            Id: itemId,
+            Quantidade: novaQtde
+        };
+    }
+
+    postQuantidade(data) {
+
+        //Chamada AJAX
+        $.ajax({
+            url: '/pedido/updatequantidade',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(data) /*Transformei o objeto data em string */
+        });
+
+    }
+
+
+
+
+}
+
+
+//Escopo global
+var carrinho = new Carrinho();
+
+
