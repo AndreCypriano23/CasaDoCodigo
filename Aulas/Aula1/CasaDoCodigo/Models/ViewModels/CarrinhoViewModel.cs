48eccec3ff3cbe216fd8e895ff8ebb083d185825
AndreCypriano23 <andrecypriano23@outlook.com>
2022-11-19 11:42:50 -0300

    ViewModel criada

---
+++ Aulas/Aula1/CasaDoCodigo/Models/ViewModels/CarrinhoViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Models.ViewModels
+{
+    public class CarrinhoViewModel
+    {
+        public CarrinhoViewModel(IList<ItemPedido> itens)
+        {
+            this.Itens = itens;
+        }
+
+        public IList<ItemPedido> Itens { get; }//nao vou colocar set pq ela é somente leitura
+
+        public decimal Total => Itens.Sum(i => i.Quantidade * i.PrecoUnitario);
+
+
+    }
+}
